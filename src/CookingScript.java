f9794eafd3f36a96e112b306cde821d960c60de2
bulletmagnet <wildnate@gmail.com>
2021-09-05 21:41:00 -0700

    Added first ever GUI that starts script and chooses what to cook.

--- src/CookingScript.java
+++ src/CookingScript.java
@@ -19,15 +19,21 @@ import org.dreambot.api.utilities.Timer;
 import org.dreambot.api.wrappers.interactive.interact.Interactable;
 import org.dreambot.api.wrappers.widgets.message.Message;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
 
 @ScriptManifest(name = "Bullets Cooker", description = "Cooks fish", author = "Bulletmagnet",
         version = 1.0, category = Category.COOKING, image = "")
-public class CookingScript extends AbstractScript implements ChatListener {
+public class CookingScript extends AbstractScript implements ChatListener, ActionListener {
+
 
     final Area CookingRange = new Area(3212, 3216, 3210, 3213, 0);
-
+    String RawAnchovies = "Raw anchovies";
+    String RawShrimps = "Raw shrimps";
+    String CoookedFish = "Anchovies";
     private Timer timeRan;
     int fishCooked = 0;
     int beginningXP;
@@ -39,41 +45,68 @@ public class CookingScript extends AbstractScript implements ChatListener {
     private final BasicStroke stroke1 = new BasicStroke(5);
     private DrawMouseUtil drawMouseUtil = new DrawMouseUtil();
 
+    public String getFish() {
+        return fish;
+    }
+
+    public void setFish(String fish) {
+        this.fish = fish;
+    }
+
+    public String fish;
+
+    public boolean isStart() {
+        return Start;
+    }
+
+    public void setStart(boolean start) {
+        Start = start;
+    }
+
+    public boolean Start;
+
     public void onStart() {
+        GUI();
         drawMouseUtil.setRandomColor();
         SkillTracker.start(Skill.COOKING);
         beginningXP = Skills.getExperience(Skill.COOKING);
         Timer TTLV = new Timer();
         timeRan = new Timer();
-        if (Inventory.isEmpty()) {
-            bank();
-        } else if (Inventory.contains("Raw shrimps")) {
-            cook();
+        log("Waiting for gui to say start");
+        if (Start) {
+            if (Inventory.isEmpty()) {
+                bank();
+            } else if (Inventory.contains(fish)) {
+                cook();
+            }
+            log("Starting Bullets Cooker");
         }
-        log("Starting Bullets Cooker");
     }
+
 
     @Override
     public int onLoop() {
-        if (Inventory.isEmpty()){
-            bank();
-        } else {
-            cook();
+        if (Start) {
+            if (Inventory.isEmpty()) {
+                bank();
+            } else {
+                cook();
+            }
         }
-
         return 0;
     }
+
     public void onGameMessage(Message message) {
-        if (message.getMessage().contains("You successfully cook some shrimps")) {
+        if (message.getMessage().contains("You successfully cook some anchovies")) {
             fishCooked++;
         }
     }
 
     public void cook() {
-        if (!CookingRange.contains(getLocalPlayer()) && Inventory.contains("Raw shrimps")) {
+        if (!CookingRange.contains(getLocalPlayer()) && Inventory.contains(getFish())) {
             Walking.walk(CookingRange);
             sleep(1000, 4500);
-        } else if (CookingRange.contains(getLocalPlayer()) && Inventory.contains("Raw shrimps")) {
+        } else if (CookingRange.contains(getLocalPlayer()) && Inventory.contains(getFish())) {
             sleep(1000, 3000);
             GameObjects.closest("Cooking range").interact("Cook");
             sleep(1000, 3000);
@@ -88,20 +121,21 @@ public class CookingScript extends AbstractScript implements ChatListener {
     }
 
     public void bank() {
-        if (Inventory.isEmpty() || Inventory.contains("Shrimps")) {
+        if (Inventory.isEmpty() || Inventory.contains(CoookedFish)) {
             Walking.walk(BankLocation.LUMBRIDGE.getCenter());
             sleep(1000, 2500);
             Bank.openClosest();
             sleep(500, 1200);
             Bank.depositAllItems();
             sleep(500, 1000);
-            Bank.withdraw("Raw shrimps", 28);
+            Bank.withdraw(getFish(), 28);
             sleep(500, 1000);
             Bank.close();
         } else {
             cook();
         }
     }
+
     @Override
     public void onPaint(Graphics2D g) {
         drawMouseUtil.drawRandomMouse(g);
@@ -124,18 +158,19 @@ public class CookingScript extends AbstractScript implements ChatListener {
         g.setColor(color2);
         g.setStroke(stroke1);
         g.setColor(color3);
-        g.drawString("Bullets Lumby shrimp cooker", 110, 280);
+        g.drawString("Bullets fish cooker", 110, 280);
         g.drawString("current xp: " + currentXp, 200, 450);
         g.drawString("Things Cooked Successfully: " + fishCooked, 130, 425);
         g.drawString("Time Ran: " + timeRan.formatTime(), 200, 400);
         g.drawString("XP GAINED: " + xpGained, 200, 375);
         g.drawString("Current level: " + Skills.getRealLevel(Skill.COOKING), 200, 350);
-        g.drawString("Time tell level: " + ft(timeTNL), 20,300);
+        g.drawString("Time tell level: " + ft(timeTNL), 20, 300);
+
 
 
     }
-    private String ft(long duration)
-    {
+
+    private String ft(long duration) {
         String res = "";
         long days = TimeUnit.MILLISECONDS.toDays(duration);
         long hours = TimeUnit.MILLISECONDS.toHours(duration)
@@ -152,6 +187,60 @@ public class CookingScript extends AbstractScript implements ChatListener {
             res = (days + ":" + hours + ":" + minutes + ":" + seconds);
         }
         return res;
+    }
+
+    public void GUI (){
+        JFrame frame = new JFrame();
+        frame.setTitle("FishSettings");
+        frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setSize(180, 160);
+        frame.setVisible(true);
+        JButton start = new JButton();
+        start.addActionListener(this);
+        start.setText("START");
+        JButton shrimp = new JButton();
+        JButton anchovies = new JButton();
+        shrimp.setText("Shrimp");
+        anchovies.setText("Anchovies");
+        start.setBounds(0,10, 50, 50);
+        shrimp.setBounds(50, 10, 50, 50);
+        anchovies.setBounds(100, 10, 60, 50);
+        shrimp.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SetShrimps();
+            }
+        });
+        anchovies.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SetAnchovies();
+            }
+        });
+
+
+        frame.add(start);
+        frame.add(shrimp);
+        frame.add(anchovies);
+        frame.getContentPane().setLayout(null);
+
+
+    }
+    public void SetShrimps(){
+        setFish(RawShrimps);
+        log("Set fish to cook to Raw Shrimps");
+    }
+    public void SetAnchovies(){
+        setFish(RawAnchovies);
+        log("Set fish to cook to Raw Anchovies");
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setStart(true);
+
+
     }
 }
 
